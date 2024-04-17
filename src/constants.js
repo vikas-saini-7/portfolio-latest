1dd97fba8800d3727a942ba8367759d534710d95
vikas-saini-7 <vikas289888@gmail.com>
2024-04-17 23:35:01 +0530

    project 3 added

--- src/constants.js
+++ src/constants.js
@@ -13,15 +13,27 @@ export const ProjectsData = [
         image: 'images/projects/project-1.png'
     },
     {
-        id: 1,
+        id: 2,
         type: 'Club Website',
         name: 'Acunetix 11.0',
         tech: ["NextJS", "Tailwind CSS"],
-        description: ["Helped seniors in developing clubs website."],
+        description: ["Helped seniors in developing Acunetix Event website."],
         links: {
             live: 'https://www.acunetix11.tech',
             github: 'https://github.com/vikas-saini-7/Acunetix11'
         },
         image: 'images/projects/project-2.png'
+    },
+    {
+        id: 3,
+        type: 'Self Learning',
+        name: 'CoinCap - Crypto',
+        tech: ["HTML","SCSS" ,"Tailwind CSS", "ReactJS"],
+        description: ["Created web app that can be used to view cryptocurrency details."],
+        links: {
+            live: 'https://coincap-crypto.vercel.app',
+            github: 'https://github.com/vikas-saini-7/coincap-crypto'
+        },
+        image: 'images/projects/project-3.png'
     },
 ]
